98e90e0c76c3b3f8f26ffc17ca62fee591012b30
LucaFabbozzo <lucafabbozzo@gmail.com>
2022-11-08 13:33:56 -0500

    [add] milestone 2

--- milestone2/js/script.js
+++ milestone2/js/script.js
@@ -2,11 +2,6 @@
 // ● Visualizzazione dinamica dei messaggi: tramite la direttiva v-for, visualizzare tutti i messaggi relativi al contatto attivo all’interno del pannello della conversazione
 // ● Click sul contatto mostra la conversazione del contatto cliccato
 
-// Milestone 3
-// ● Aggiunta di un messaggio: l’utente scrive un testo nella parte bassa e digitando
-// “enter” il testo viene aggiunto al thread sopra, come messaggio verde
-// ● Risposta dall’interlocutore: ad ogni inserimento di un messaggio, l’utente riceverà
-// un “ok” come risposta, che apparirà dopo 1 secondo.
 
 
 const { createApp } = Vue;
@@ -23,17 +18,17 @@ createApp({
             {
               date: '15:30',
               message: 'Hai portato a spasso il cane?',
-              status: true
+              status: 'sent'
             },
             {
               date: '15:50',
               message: 'Ricordati di stendere i panni',
-              status: true
+              status: 'sent'
             },
             {
               date: '16:15',
               message: 'Tutto fatto!',
-              status: false
+              status: 'received'
             }
           ],
         },
